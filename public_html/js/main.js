7393bdf4679a4f0b882a2f34972db8b1f31c9502
lartiend <31193877+lartiend@users.noreply.github.com>
2017-11-16 21:53:07 +0800

    Add files via upload

---
+++ public_html/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("button#seeResponsesBtn").click(function(){
+		$("div#seeResponses").toggle();
+	});
+
+	// $('a[data-toggle="collapse"]').click(function() {
+	// 	var val = $('#card_articleID').val();
+	// 	console.log(val);
+	//   $("div[id*='article_card_]"+val+"'").css('border-color', 'green');
+	// });
+
+});
