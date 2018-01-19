c2c782ce9b4e7144db6d29f240782b2b49246f47
lartiend <31193877+lartiend@users.noreply.github.com>
2018-01-20 00:59:51 +0800

    Fix issue for 'username' duplicates

--- median/app/Http/Controllers/Auth/RegisterController.php
+++ median/app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'username' => 'required',
+            'username' => 'required|unique:users',
         ]);
     }
 
